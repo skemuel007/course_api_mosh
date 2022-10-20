16ed75ed943e949146cf761f5c3e5d4271180bb9
salvationlloydstanley-kemuel <skemuel008@gmail.com>
2022-10-20 08:37:21 +0100

    CRUD completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const express = require('express');
+const { send } = require('express/lib/response');
 const app = express();
 
 app.use(express.json())
@@ -7,32 +8,31 @@ app.use(express.json())
 const courses = [
     { id: 1, name: 'Course 1'},
     { id: 2, name: 'Course 2'},
-    { id: 2, name: 'Course 3'}
+    { id: 3, name: 'Course 3'}
 ];
 
 app.get('/',(req, res) => {
-    res.send('Hello World');
+    return res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send(courses);
+    return res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id == parseInt(req.params.id));
     if (!course)
-        res.status(404).send('The course with the given Id was not found');
-    res.send(course);
+        return res.status(404).send('The course with the given Id was not found');
+    return res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
 
     // step 1 define a schema
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.ValidationError(req.body, schema);
+    const { error } = validateCourse(req.body); // same as result.error
+    if ( error ) {
+        return res.status(400).send(error.details[0].message);
+    }
 
     // console.log(result);
     /*if (!req.body.name || req.body.name.length < 3) {
@@ -45,7 +45,44 @@ app.post('/api/courses', (req, res) => {
 
     courses.push(course);
     return res.status(200).send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+
+    const { error } = validateCourse(req.body); // same as result.error
+    if ( error ) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send('Course not found');
+
+    course.name = req.body.name;
+
+    return res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    // validate course exists
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send('Course not found');
+
+    // delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    return res.send(course);
 })
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+};
 
 const port = process.env.PORT || 3000;
 
